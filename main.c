d69520cab3d3f35ccef6806fab6cfa0e5c96aed6
PrathameshGund <96653014+PrathameshGund@users.noreply.github.com>
2022-11-23 19:50:08 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int marks[5];
+     for(int i=0;i>5;i++)
+     {
+         printf("Enter the number %d:",i+1);
+         scanf("%d",&marks[i]);
+     }
+
+    return 0;
+}
